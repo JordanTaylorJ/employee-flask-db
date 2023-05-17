41355ff6cd9c64fd1db57543d4cabc63151a5468
JordanJoseph <jordant.joseph@outlook.com>
2023-05-17 15:08:14 -0600

    import sqlalchemy ORM and flask_restful for routing

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello!"
+
+#@app.route('/')
+#def hello():
+#    return "Hello!"
+
